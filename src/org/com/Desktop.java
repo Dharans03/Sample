e552aa367895202bbc53f21ded61001cfc30d257
Dharans03 <dharanya1423@gmail.com>
2022-05-20 02:15:56 -0400

    Madhu

---
+++ src/org/com/Desktop.java
@@ -0,0 +1,12 @@
+package org.com;
+
+public class Desktop extends Computer {
+	private void desktopSize() {
+		System.out.println("909");
+}
+	public static void main(String[] args) {
+		Desktop d = new Desktop();
+		d.desktopSize();
+		d.computerModel();
+	}
+}
